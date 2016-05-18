1285491cb15776be54f2ef991417358c39e5da2c
Stephen Lautier <stephen.lautier@outlook.com>
2016-05-18 13:06:20 +0200

    chore(typings): removed typings so that it will be inferred automagically

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,3 +1,1 @@
 /// <reference path="browser/ambient/es6-shim/index.d.ts" />
-/// <reference path="browser/definitions/ssv-core/index.d.ts" />
-/// <reference path="browser/definitions/ssv-ng2-core/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,3 +1,1 @@
 /// <reference path="main/ambient/es6-shim/index.d.ts" />
-/// <reference path="main/definitions/ssv-core/index.d.ts" />
-/// <reference path="main/definitions/ssv-ng2-core/index.d.ts" />
