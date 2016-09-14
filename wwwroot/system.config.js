e53406791cb95c7c0060e39d99f6eb9135954260
Stephen Lautier <stephen.lautier@outlook.com>
2016-09-15 00:53:26 +0200

    fixed ionic-angular runtime issues

--- wwwroot/system.config.js
+++ wwwroot/system.config.js
@@ -42,16 +42,20 @@
             main: "dist/amd/index.js",
             defaultExtension: "js"
         },
+        "ionic-angular": {
+            main: "commonjs/index.js",
+            defaultExtension: "js"
+        },
         '@angular/core' : {main: 'bundles/core.umd.min.js'},
         '@angular/common' : {main: 'bundles/common.umd.min.js'},
         '@angular/forms' : {main: 'bundles/forms.umd.min.js'},
+        '@angular/http' : {main: 'bundles/http.umd.min.js'},
         '@angular/platform-browser' : {main: 'bundles/platform-browser.umd.min.js'},
         '@angular/platform-browser-dynamic' : {main: 'bundles/platform-browser-dynamic.umd.min.js'},
         '@angular/router' : {main: 'bundles/router.umd.min.js'},
     };
 
     var packageNames = [
-        "ionic-angular",
         "lodash",
     ];
 
