e80741a9d4f5cae1e01bfa947d075ad6f015857c
Pawlu Frendo <pawlu.frendo@betssongroup.com>
2016-09-15 01:33:05 +0200

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { IonicModule, IonicApp } from "ionic-angular";
+import { IonicModule, Config } from "ionic-angular";
 
 import { TabsPage } from "./pages/tabs/tabs";
 import { HomePage } from "./pages/home/home";
@@ -7,7 +7,6 @@ import { AboutPage } from "./pages/about/about";
 import { ContactPage } from "./pages/contact/contact";
 import { deepLinkConfig } from "./shared/route.config";
 import { NavComponent } from "./shared/nav.component"
-import { MyApp } from "./app";
 
 @NgModule({
   declarations: [
@@ -19,7 +18,9 @@ import { MyApp } from "./app";
   imports: [
     IonicModule.forRoot(NavComponent, null, deepLinkConfig)
   ],
-  bootstrap: [IonicApp, MyApp],
+  bootstrap: [
+    NavComponent
+  ],
   entryComponents: [
     TabsPage,
     HomePage,
