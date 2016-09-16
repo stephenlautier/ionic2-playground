cb5626c658f2ac520509a618d447b24bea967df0
Pawlu Frendo <pawlu.frendo@betssongroup.com>
2016-09-16 10:05:50 +0200

    added scenarios

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,12 +2,15 @@ import { App } from "ionic-angular";
 import { Component } from '@angular/core';
 
 import { AboutPage } from "../about/about";
+import { ContactPage } from "../contact/contact";
 
 @Component({
 	moduleId: module.id,
 	templateUrl: 'home.html'
 })
 export class HomePage {
+
+	component = ContactPage;
 
 	constructor(
 		public app: App
