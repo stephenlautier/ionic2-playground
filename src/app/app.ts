1fee80dd134c2bd6f83a2178b2f034be7ce87294
Pawlu Frendo <pawlu.frendo@betssongroup.com>
2016-09-16 08:42:30 +0200

    fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,4 @@
-import { Component, ViewChild } from "@angular/core";
-import { Nav } from "ionic-angular";
-
-import { TabsPage } from "./pages/tabs/tabs";
-import { HomePage } from "./pages/home/home";
-import { AboutPage } from "./pages/about/about";
-import { RouterService, RouteModel } from "./shared/index";
+import { Component } from "@angular/core";
 
 @Component({
 	moduleId: module.id,
@@ -12,26 +6,9 @@ import { RouterService, RouteModel } from "./shared/index";
 })
 export class MyApp {
 
-	@ViewChild("myNav") nav: Nav;
-
-	private rootPage: any;
-	private routes: RouteModel[] = [{
-		component: HomePage,
-		path: "#/home"
-	}, {
-		component: AboutPage,
-		path: "#/about"
-
-	}];
-
 	constructor(
-		private routerService: RouterService
 	) {
 		// this.rootPage = HomePage;
-		this.rootPage = TabsPage;
-
-		routerService.mapRoutes(this.routes);
-		routerService.initialize();
 
 		// platform.ready().then(() => {
 		//   // Okay, so the platform is ready and our plugins are available.
